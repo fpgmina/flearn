c9d603d052ffdbd925a7f42d6cf929610905b8c0
fpgmina <francesco.mina1986@gmail.com>
2025-04-28 17:12:47 +0300

    add freeze_parameter argument in get_dino_backbone_model function

--- models/dino_backbone.py
+++ models/dino_backbone.py
@@ -4,14 +4,15 @@ import torch.nn as nn
 from utils.model_utils import get_device
 
 
-def get_dino_backbone_model(num_classes: int = 100):
+def get_dino_backbone_model(num_classes: int = 100, freeze_backbone: bool = True):
     device = get_device()
     backbone = timm.create_model(
         "vit_small_patch16_224_dino", pretrained=True
     )  # TODO check this is the dino model we are supposed to use
 
-    for param in backbone.parameters():
-        param.requires_grad = False
+    if freeze_backbone:
+        for param in backbone.parameters():
+            param.requires_grad = False
 
     backbone.head = nn.Linear(384, num_classes)
     return backbone.to(device)
