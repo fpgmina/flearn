433bdcbfdae675cf32c34e38d3426209a4576c5f
fpgmina <francesco.mina1986@gmail.com>
2025-05-26 19:07:23 +0200

    remove TODO in bin/fedav_model_noniid

--- bin/train_federated_averaging_model_non_iid_editing.py
+++ bin/train_federated_averaging_model_non_iid_editing.py
@@ -34,8 +34,8 @@ if __name__ == "__main__":
     # Load model with editable backbone and modified head to fit on CIFAR100
     model = get_dino_backbone_model(freeze_backbone=False)
     mask = Mask.load_state_dict(torch.load(mask_path))
-    # TODO: cambia la head della mask cosi' che abbia 1s in modo che sia consistente con il model.
-    #  (model ha random weights nella head, voglio poterli update)
+    # cambia la head della mask cosi' che abbia 1s in modo che sia consistente con il model.
+    # (model ha random weights nella head, voglio poterli update)
     mask = mask.unmask_layers(["head.weight", "head.bias"])
 
     device = get_device()
